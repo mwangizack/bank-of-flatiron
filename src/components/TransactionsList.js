0750f3279d6287b614e1ba502d046adaebf23211
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-05-29 19:55:06 +0300

    feat: Add a view transactions feature on load

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList() {
+  const [transactionsData, setTransactionsData] = useState([]);
+
+  fetch("http://localhost:3000/transactions")
+    .then((response) => response.json())
+    .then((data) => setTransactionsData(data))
+    .catch((error) => console.log(`Error fetching data: ${error}`));
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +26,15 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {transactionsData.map((transactionData) => (
+          <Transaction
+            key={transactionData.id}
+            date={transactionData.date}
+            description={transactionData.description}
+            category={transactionData.category}
+            amount={transactionData.amount}
+          />
+        ))}
       </tbody>
     </table>
   );
